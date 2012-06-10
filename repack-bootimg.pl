3c11e11e0584327b3bd70967d526458e8095737d
user <user@dellpc-lnx.(none)>
2012-06-10 10:12:58 +0800

    modification of repack for n780

--- repack-bootimg.pl
+++ repack-bootimg.pl
@@ -16,7 +16,7 @@ system ("find . | cpio -o -H newc | gzip > $dir/ramdisk-repack.cpio.gz");
 
 chdir $dir or die "$ARGV[1] $!";;
 
-system ("mkbootimg --cmdline 'no_console_suspend=1 console=null' --kernel $ARGV[0] --ramdisk ramdisk-repack.cpio.gz -o $ARGV[2]");
+system ("/mkbootimg --kernel $ARGV[0] --ramdisk ramdisk-repack.cpio.gz --base 0x02600000 -o $ARGV[2]");
 
 unlink("ramdisk-repack.cpio.gz") or die $!;
 
